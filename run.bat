a30574de334d702a1d9034c381291f5a83957181
beeczxd <beecz@drcoconut.net>
2025-11-07 19:37:29 +0100

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off 
+amidewinx64.exe /IV %random%%random%%random%%random%
+amidewinx64.exe /ID %random%%random%%random%%random%
+amidewinx64.exe /SM %random%%random%%random%%random%
+amidewinx64.exe /SP %random%%random%%random%%random%
+amidewinx64.exe /SV %random%%random%%random%%random%
+amidewinx64.exe /SS %random%%random%%random%%random%
+amidewinx64.exe /SU %random%%random%%random%%random%
+amidewinx64.exe /BM %random%%random%%random%%random%
+amidewinx64.exe /BP %random%%random%%random%%random%
+amidewinx64.exe /BS %random%%random%%random%%random%
+amidewinx64.exe /CM %random%%random%%random%%random%
+amidewinx64.exe /CS %random%%random%%random%%random%
