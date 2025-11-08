092ff629fe5bde736773cde599c35352eeb11320
beeczxd <beecz@drcoconut.net>
2025-11-08 21:38:46 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,16 @@
 @echo off 
-C:\Users\%username%\ajotak\amidewinx64.exe /IV %random%%random%%random%%random%
-C:\Users\%username%\ajotak\amidewinx64.exe /ID %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BS %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /SM %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /SP %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /SV %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /SS %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /SU %random%%random%%random%%random%
-C:\Users\%username%\ajotak\amidewinx64.exe /BM %random%%random%%random%%random%
-C:\Users\%username%\ajotak\amidewinx64.exe /BP %random%%random%%random%%random%
-C:\Users\%username%\ajotak\amidewinx64.exe /BS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SK %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SF %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BLC %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /CM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CV %random%%random%%random%%random%
 C:\Users\%username%\ajotak\amidewinx64.exe /CS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CA %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CSK %random%%random%%random%%random%
