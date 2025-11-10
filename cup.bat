c4f3e2c7c22c8a0bf3c1c421d16c6bcaeff18916
beeczxd <beecz@drcoconut.net>
2025-11-10 17:20:33 +0100

    Create cup.bat

---
+++ cup.bat
@@ -0,0 +1,26 @@
+@echo off
+C:\Users\%username%\ajotak\amidewinx64.exe /IVN %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /IV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SP %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SU %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SK %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SF %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PL %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CA %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CSK %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PSN To be filled by O.E.M
+C:\Users\%username%\ajotak\amidewinx64.exe /PAT To be filled by O.E.M
+C:\Users\%username%\ajotak\amidewinx64.exe /PPN To be filled by O.E.M
