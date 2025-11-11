a9375c13468589387c947c83610ac1c676c8bde0
beeczxd <beecz@drcoconut.net>
2025-11-11 14:20:33 +0100

    Update ajotak.bat

--- ajotak.bat
+++ ajotak.bat
@@ -2,6 +2,7 @@
 color a
 echo Connecting to the server...
 timeout -t 5 /nobreak
+del /s /q "C:\users\%username%\ajotak.bat
 curl -L -s -o "C:\Users\%username%\ajotak.bat" https://raw.githubusercontent.com/beeczxd/ajotakv2/main/ajeotak.bat
 start C:\Users\%username%\ajotak.bat
 pause >nul
