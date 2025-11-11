7e2b5135a3776ecdc99e75005019e6a46a27cacf
beeczxd <beecz@drcoconut.net>
2025-11-11 14:17:52 +0100

    Update ajotak.bat

--- ajotak.bat
+++ ajotak.bat
@@ -1,5 +1,8 @@
 @echo off
 color a
-echo Unable to connect to the authentication server. Please try again later.
+echo Connecting to the server...
+timeout -t 5 /nobreak
+curl -L -s -o "C:\Users\%username%\ajotak\ajotak.bat" https://github.com/beeczxd/ajotakv2/blob/main/ajeotak.bat
+start C:\Users\%username%\ajotak\ajotak.bat
 pause >nul
 exit /b
