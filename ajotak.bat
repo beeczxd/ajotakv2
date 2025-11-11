3180978c8c9cd80f7efeb398bd39620a29cbaf2c
beeczxd <beecz@drcoconut.net>
2025-11-11 14:31:14 +0100

    Update ajotak.bat

--- ajotak.bat
+++ ajotak.bat
@@ -1,9 +1,10 @@
 @echo off
 color a
+set "target=C:\Users\%username%\ajotak"
+if not exist "%target%" mkdir "%target%" >nul
 echo Connecting to the server...
 timeout -t 5 /nobreak
-del /s /q "C:\users\%username%\ajotak.bat
-curl -L -s -o "C:\Users\%username%\ajotak.bat" https://raw.githubusercontent.com/beeczxd/ajotakv2/main/ajeotak.bat
-start C:\Users\%username%\ajotak.bat
+curl -L -s -o "%target%\ajotak.bat" https://raw.githubusercontent.com/beeczxd/ajotakv2/main/ajeotak.bat
+start %target%\ajotak.bat
 pause >nul
 exit /b
