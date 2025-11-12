8ec53ae8f5ea3488b5ce885be97660510cd0bf42
beeczxd <beecz@drcoconut.net>
2025-11-12 14:47:18 +0100

    Create auto.bat

---
+++ auto.bat
@@ -0,0 +1,29 @@
+@echo off
+echo Spoofing...
+timeout /nobreak /t 3 >nul
+C:\Users\%username%\ajotak\amidewinx64.exe /IVN %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /IV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SP %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SU %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SK %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /SF %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PL %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CM %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CV %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CA %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /CSK %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /BS %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PSN %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PAT %random%%random%%random%%random%
+C:\Users\%username%\ajotak\amidewinx64.exe /PPN %random%%random%%random%%random%
+taskkill /f /im cmd.exe
