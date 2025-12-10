88dcab39bf99abab24366bc9dc7d38b41c8a0a24
beeczxd <beecz@drcoconut.net>
2025-12-10 22:01:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/lb', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   try {
     const apiResponse = await fetch(
-      'https://api.raw365.gg/v2/affiliate-api/leaderboard?apiKey=75e83a38-8d27-4806-8735-7c3471874375&start=2025-12-10&end=2025-12-11'
+      'https://api.raw365.gg/v2/affiliate-api/leaderboard?apiKey=75e83a38-8d27-4806-8735-7c3471874375&start=2025-12-10T00:00:00&end=2025-12-15T23:59:59'
     );
     const data = await apiResponse.json();
     res.json(data);
